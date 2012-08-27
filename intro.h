f10753d9b58812cc722240b8415a184c111591d5
Ziz <zizsdl@googlemail.com>
2012-08-27 11:52:52 +0200

    Intro is done

--- intro.h
+++ intro.h
@@ -2,21 +2,18 @@ int intro_pos=19000;
 
 void draw_intro(void)
 {
-  spClearTarget(12345);
+  spClearTarget(0);
   
-  /*drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-9400)*(engineGetWindowY()>>6))>>7),"There was the big war.",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-8600)*(engineGetWindowY()>>6))>>7),"The evil flat rotating",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-7800)*(engineGetWindowY()>>6))>>7),"circles took the power.",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-7000)*(engineGetWindowY()>>6))>>7),"But the resistance movement",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-6200)*(engineGetWindowY()>>6))>>7),"fought against the intruders.",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-5400)*(engineGetWindowY()>>6))>>7),"That was a long time ago.",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-4600)*(engineGetWindowY()>>6))>>7),"The movement is exhausted,",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-3800)*(engineGetWindowY()>>6))>>7),"but the enemies are still there.",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-3000)*(engineGetWindowY()>>6))>>7),"You are the last (snow)man.",engineGetSurface(SURFACE_KEYMAP));
-  drawtextMXMY(engineGetSurface(SURFACE_SURFACE),engineGetWindowX()>>1,(((intro_pos-2200)*(engineGetWindowY()>>6))>>7),"Bring it to an end.",engineGetSurface(SURFACE_KEYMAP));*/
-  char buffer[256];
-	sprintf( buffer, "Snowman is a game about... snowmen!\nfps: %i", spGetFPS() );
-	spFontDrawMiddle( screen->w/2, 1, -1, buffer, font );
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-9400)*(screen->h>>6))>>7),-1,"There was the big war.",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-8600)*(screen->h>>6))>>7),-1,"The evil flat rotating",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-7800)*(screen->h>>6))>>7),-1,"circles took the power.",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-7000)*(screen->h>>6))>>7),-1,"But the resistance movement",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-6200)*(screen->h>>6))>>7),-1,"fought against the intruders.",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-5400)*(screen->h>>6))>>7),-1,"That was a long time ago.",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-4600)*(screen->h>>6))>>7),-1,"The movement is exhausted,",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-3800)*(screen->h>>6))>>7),-1,"but the enemies are still there.",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-3000)*(screen->h>>6))>>7),-1,"You are the last (snow)man.",font);
+  spFontDrawMiddle( screen->w/2 ,(((intro_pos-2200)*(screen->h>>6))>>7),-1,"Bring it to an end.",font);
   spFlip();
 }
 
@@ -26,7 +23,9 @@ int calc_intro(Uint32 steps)
   intro_pos-=steps;
   if (intro_pos<=0)
     return 1;
-  if (engineInput->button[SP_BUTTON_LEFT] || engineInput->button[SP_BUTTON_RIGHT] || engineInput->button[SP_BUTTON_DOWN] || engineInput->button[SP_BUTTON_UP])
+  if (engineInput->button[SP_BUTTON_LEFT] || engineInput->button[SP_BUTTON_RIGHT] ||
+			engineInput->button[SP_BUTTON_DOWN] || engineInput->button[SP_BUTTON_UP] ||
+			engineInput->button[SP_BUTTON_START] || engineInput->button[SP_BUTTON_SELECT])
     return 1;
   
   return 0;
