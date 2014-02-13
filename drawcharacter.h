32979201e3a009a10faf9fcf747af9869a6a744d
Ziz <zizsdl@googlemail.com>
2014-02-13 17:26:29 +0100

    Bugs + License

--- drawcharacter.h
+++ drawcharacter.h
@@ -1,3 +1,20 @@
+ /* This file is part of snowman.
+  * Snowman is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  * 
+  * Snowman is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with snowman.  If not, see <http://www.gnu.org/licenses/>
+  * 
+  * For feedback and questions about my Files and Projects please mail me,
+  * Alexander Matthes (Ziz) , zizsdl_at_googlemail.com */
+  
 char removesnow(int count)
 {
 	if (ballcount==1)
