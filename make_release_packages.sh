b2fb8e2423a80189c485462add8d27ee0f74ea9a
Ziz <zizsdl@googlemail.com>
2014-09-07 15:21:43 +0200

    Fixed a C4A bug

--- make_release_packages.sh
+++ make_release_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Change the folder to YOUR sparrow3d folder!
 PROGRAM="snowman"
-VERSION="2.0.0.16"
+VERSION="2.0.0.17"
 DEST=./build/*
 echo "<html>" > index.htm
 echo "<head>" >> index.htm
